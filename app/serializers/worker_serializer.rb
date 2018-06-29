b57d7200d82b7a182895ce80a761ae2d986bded3
neimad971 <damientalbot26@gmail.com>
2018-06-29 15:23:26 +0200

    more data(status, shifts_count, shifts_dates) are renderered for the frontend

--- app/serializers/worker_serializer.rb
+++ app/serializers/worker_serializer.rb
@@ -1,7 +1,15 @@
 class WorkerSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :price
+  attributes :id, :first_name, :status, :price, :shifts_count, :shifts_dates
 
   def price
     object.salary
+  end
+
+  def shifts_count
+    object.shifts.count
+  end
+
+  def shifts_dates
+    object.shifts.map { |shift| shift.start_date }
   end
 end
